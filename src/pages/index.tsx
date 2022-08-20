1e0257a25d2e012822fbcc27c9077449f2f116a3
Sindre Bakken <sindre.bakken.naesset@gmail.com>
2022-08-20 17:35:42 +0200

    update test query on client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 import { trpc } from '../utils/trpc';
-import { NextPageWithLayout } from './_app';
-import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 
 export default function Home() {
   const { data: session, status } = useSession();
-  const utils = trpc.useContext();
-  const welcomeQuery = trpc.useQuery(['hello', { text: session?.user?.name }]);
-
-  // prefetch all posts for instant navigation
-  // useEffect(() => {
-  //   for (const { id } of postsQuery.data ?? []) {
-  //     utils.prefetchQuery(['post.byId', { id }]);
-  //   }
-  // }, [postsQuery.data, utils]);
+  const welcomeQuery = trpc.useQuery(['hello', { text: session?.user?.name }], {
+    enabled: !!session?.user?.name,
+  });
   if (!welcomeQuery.data) {
     return <div>Loading...</div>;
   }
@@ -22,4 +14,4 @@ export default function Home() {
       <h1>{welcomeQuery.data.greeting}</h1>
     </>
   );
-};
+}
